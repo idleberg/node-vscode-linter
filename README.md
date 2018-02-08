281f82c721f1e6d99edcfe19c584ea18b8e8b72a
idleberg <jan@idleberg.com>
2018-02-08 16:41:15 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# vscode-linter
+
+[![npm](https://img.shields.io/npm/l/vscode-linter.svg?style=flat-square)](https://www.npmjs.org/package/vscode-linter)
+[![npm](https://img.shields.io/npm/v/vscode-linter.svg?style=flat-square)](https://www.npmjs.org/package/vscode-linter)
+[![David](https://img.shields.io/david/idleberg/node-vscode-linter.svg?style=flat-square)](https://david-dm.org/idleberg/node-vscode-linter)
+
+Linter for VSCode extensions, supporting language syntax, snippets, themes and more.
+
+## Usage
+
+Install `vscode-linter` dependency for your VSCode extension
+
+```sh
+yarn add vscode-linter --dev || npm install vscode-linter -D
+```
+
+## License
+
+This work is licensed under [The MIT License](https://opensource.org/licenses/MIT)
+
+## Donate
+
+You are welcome support this project using [Flattr](https://flattr.com/submit/auto?user_id=idleberg&url=https://github.com/idleberg/node-vscode-linter) or Bitcoin `17CXJuPsmhuTzFV2k4RKYwpEHVjskJktRd`
---
+++ index.js
@@ -0,0 +1,71 @@
+#!/usr/bin/env node
+
+// Dependencies
+const gulp = require('gulp');
+const debug = require('gulp-debug');
+// const esLint = require('gulp-eslint');
+const jsonLint = require('gulp-json-lint');
+const tsLint = require('gulp-tslint');
+const xmlValidator = require('gulp-xml-validator');
+
+// Gulp options
+const options = {
+  allowEmpty: true
+};
+
+const src = {
+  json: [
+    'package.json',
+    'snippets/*.json',
+    'tsconfig.json',
+    'tslint.json',
+    '!bower_components/**/*',
+    '!node_modules/**/*'
+  ],
+  // javascript: [
+  //   '**/*.js',
+  //   '!bower_components/**/*',
+  //   '!node_modules/**/*'
+  // ],
+  typescript: [
+    '**/*.ts',
+    '!bower_components/**/*',
+    '!node_modules/**/*'
+  ],
+  xml: [
+    '**/*.svg',
+    '**/*.tmLanguage',
+    '**/*.tmTheme',
+    '!bower_components/**/*',
+    '!node_modules/**/*'
+  ],
+};
+
+var jsonReporter = function (lint, file) {
+    throw `${file.path}: ${lint.error}`;
+};
+
+// Lint JSON
+gulp.src(src.json, options)
+  .pipe(debug({title: 'Lint JSON:'}))
+  .pipe(jsonLint({
+    comments: true
+  }))
+  .pipe(jsonLint.report('verbose'))
+  .pipe(jsonLint.report(jsonReporter));
+
+// gulp.src(src.javascript, options)
+//   .pipe(debug({title: 'Lint JavaScript:'}))
+//   .pipe(esLint());
+
+gulp.src(src.typescript, options)
+  .pipe(debug({title: 'Lint TypeScript:'}))
+  .pipe(tsLint({
+      formatter: "prose"
+  }))
+  .pipe(tsLint.report());
+
+// Validate XML
+gulp.src(src.xml, options)
+  .pipe(debug({title: 'Lint XML:'}))
+  .pipe(xmlValidator());
